16d20eadf365afc751fd90733c0879458e3b6a86
cloeffle <cloeffle74@gmail.com>
2022-06-28 12:31:53 +0200

    add logic for to show/hide Expense-form

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,7 +11,7 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  //* Filter to sort by year
+  //* Filter year
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
